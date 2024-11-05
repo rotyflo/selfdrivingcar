97412d51259b3f0e55a9254be13f6ce30babb44a
RT <me@rtflores.com>
2024-11-05 13:53:27 -0800

    update photoresistor code

--- code/10_2_photoresistor_code_outline/10_2_photoresistor_code_outline.ino
+++ code/10_2_photoresistor_code_outline/10_2_photoresistor_code_outline.ino
@@ -1,23 +1,23 @@
-/* Photoresistor Code Outline 
+  /* Photoresistor Code Outline 
 *
 * TODO: Locate the commented lines with "TODO:" and 
 * complete the task in the descriptions
 */
   /* Variables for Light Sensors*/
   // TODO: Replace "___", and assign the pin number connected to the Arduino
-  int LDR_Pin0 = ___; // analog pin 
-  int LDR_Pin1 = ___; // analog pin 
-  int LDR_Pin2 = ___; // analog pin 
-  int LDR_Pin3 = ___; // analog pin 
-  int LDR_Pin4 = ___; // analog pin 
-  int LDR_Pin5 = ___; // analog pin 
-  int LDR_Pin6 = ___; // analog pin 
+  int LDR_Pin0 = A0; // analog pin 
+  int LDR_Pin1 = A1; // analog pin 
+  int LDR_Pin2 = A2; // analog pin 
+  int LDR_Pin3 = A3; // analog pin 
+  int LDR_Pin4 = A4; // analog pin 
+  int LDR_Pin5 = A5; // analog pin 
+  int LDR_Pin6 = A6; // analog pin 
   
   // Initialize Photo Resistor Variables to zero
   int LDR0 = 0, LDR1 = 0, LDR2 = 0, LDR3 = 0, LDR4 = 0, LDR5 = 0, LDR6 = 0; 
  
 void setup() {
- Serial.begin(___); // TODO: Replace "___", and input the baud rate for serial communication
+ Serial.begin(9600); // TODO: Replace "___", and input the baud rate for serial communication
 }
 void loop() {
  ReadPhotoResistors(); // Read photoresistors and map to 0-100 based on calibration
@@ -29,20 +29,21 @@ void loop() {
 void ReadPhotoResistors()
 {
 // TODO: Replace "___", and use a function to read the value of the corresponding LDR Arduino pin
-  LDR0 = __;
+  LDR0 = analogRead(LDR_Pin0)/10;
     delay(2);
-  LDR1 = __;
+  LDR1 = analogRead(LDR_Pin1)/10;
     delay(2);
-  LDR2 = __;
+  LDR2 = analogRead(LDR_Pin2)/10;
     delay(2);
-  LDR3 = __;
+  LDR3 = analogRead(LDR_Pin3)/10;
     delay(2);
-  LDR4 = __;
+  LDR4 = analogRead(LDR_Pin4)/10;
     delay(2);
-  LDR5 = __;
+  LDR5 = analogRead(LDR_Pin5)/10;
     delay(2);
-  LDR6 = __;
+  LDR6 = analogRead(LDR_Pin6)/10;
     delay(2); 
+    
 }
 
 // ************************************************************************************************* //
