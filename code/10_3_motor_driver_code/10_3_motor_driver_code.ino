6b4252bcbf4b71911a6659a28f6f283fd1f39b5a
RT <me@rtflores.com>
2024-11-05 13:55:34 -0800

    update motor driver code

--- code/10_3_motor_driver_code/10_3_motor_driver_code.ino
+++ code/10_3_motor_driver_code/10_3_motor_driver_code.ino
@@ -1,71 +1,48 @@
-/* Motor Driver Code Outline */
 // Libraries for Motor
 #include <Wire.h>
 #include <Adafruit_MotorShield.h> // Must add libary - see MotorShield Manual
 //https://cdn-learn.adafruit.com/downloads/pdf/adafruit-motor-shield-v2-for-arduino.pdf
-
 // Initialize Motors
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *Motor1 = AFMS.getMotor(1); // Motors can be switched here (1) <--> (2)
 Adafruit_DCMotor *Motor2 = AFMS.getMotor(2);
-
-// Set Initial Speed of Motors (CAN BE EDITED BY USER)
-//  initial speed may vary and later can be changed with Sp potentiometer. theoretical max
-//  is 255, but the motors will likely overdraw power and cause the Arduino to shut off. 
-//  motors likely need a minimum speed of 20-30 to move the cart.
-//
-//  motor speeds are separated incase one motor turns faster than the other.  
-int M1Sp = 60; 
+//Set Initial Speed of Motors
+int M1Sp = 60; // initial speed may vary and later can be increased with Sp potentiometer
 int M2Sp = 60;
-
 //Set LED Pin
-// TODO: Replace "___", and assign the pin number connected to the Arduino.
-//  it is recommended to use pin 13, but can change to another digital pin 
-//  and connect extra LED to me more easily seen
-int led_Pin = ___; 
+int led_Pin = 13; // can change to another digital pin and connect extra LED to me more easily seen
 
 // setup - runs once
 void setup(){
-  Serial.begin(___); // TODO: Replace "___", and input the baud rate for serial communication
-  AFMS.begin(); // initialize the motor
-  
-  pinMode(___, ___); // TODO: Replace "___", and set the led_pin to be an output
-
-    // Gives you a moment before cart actually moves
-    for (int waitii = 0; waitii < 20; waitii++) {
-      digitalWrite(___, ___); // TODO: Replace "___", and turn on the LED
-      delay(100); // wait for 100 milliseconds
-
-      digitalWrite(___, ___); // TODO: Replace "___", and turn off the LED
-      delay(100); // wait for 100 milliseconds
-    } 
+  Serial.begin(9600);
+  AFMS.begin(); //for Motor
+  pinMode(led_Pin, OUTPUT); // set pin mode to output voltage
+  // Gives you a moment before tank actually moves
+  for (int waitii = 0; waitii < 20; waitii++) {
+    digitalWrite(led_Pin, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(100); // wait for 100 milliseconds
+    digitalWrite(led_Pin, LOW); // turn the LED off by making the voltage LOW
+    delay(100); // wait for 100 milliseconds
+  }
 }
-
 // loop - loops forever
-void loop(){ 
-// applicable motor keywords:
-//    FORWARD   - run the motors in the forwards direction
-//    BACKWARD  - run the motors in the backwards direction
-//    RELEASE   - stop the motors from turning
-
-  // TODO: Start Motors in forward direction
-    Motor1->setSpeed(___); // TODO: Replace "___", and set the speed of motor 1
-    Motor1->run(___); // TODO: Replace "___", and set the direction of motor 1
-    Motor2->setSpeed(___); // TODO: Replace "___", and set the speed of motor 2
-    Motor2->run(___); // TODO: Replace "___", and set the direction of motor 2
+void loop(){
+  // Start Motors in forward direction
+  Motor1->setSpeed(M1Sp);
+  Motor1->run(FORWARD);
+  Motor2->setSpeed(M2Sp);
+  Motor2->run(FORWARD);
   delay(3000); // let run forward for 3 seconds
-  
-  // TODO: Start Motors in backward direction
-    Motor1->setSpeed(___); // TODO: Replace "___", and set the speed of motor 1
-    Motor1->run(___); // TODO: Replace "___", and set the direction of motor 1
-    Motor2->setSpeed(___); // TODO: Replace "___", and set the speed of motor 2
-    Motor2->run(___); // TODO: Replace "___", and set the direction of motor 2
-  delay(3000); // let run backward for 3 seconds 
-  
-  // TODO: Stop Motors
-    Motor1->setSpeed(___); // TODO: Replace "___", and set the speed of motor 1
-    Motor1->run(___); // TODO: Replace "___", and set the direction of motor 1
-    Motor2->setSpeed(___); // TODO: Replace "___", and set the speed of motor 2
-    Motor2->run(___); // TODO: Replace "___", and set the direction of motor 2
-  delay(3000); // stop for 3 seconds 
+  // Start Motors in backward direction
+  Motor1->setSpeed(M1Sp);
+  Motor1->run(BACKWARD);
+  Motor2->setSpeed(M2Sp);
+  Motor2->run(BACKWARD);
+  delay(3000); // let run backward for 3 seconds
+  // Stop Motors
+  Motor1->setSpeed(M1Sp);
+  Motor1->run(RELEASE);
+  Motor2->setSpeed(M2Sp);
+  Motor2->run(RELEASE);
+  delay(3000); // stop for 3 seconds
 }
